411c97255f56e95b353ffdd55c93177e1cf5f278
faizanali22 <muhammadfaizan.wdc@Outlook.com>
2022-08-19 21:55:46 +0800

    Add model class Blog and Controller

---
+++ BlogApp.DAL/Models/Blog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp.DAL.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Publication_Date { get; set; }
+    }
+}
--- BlogApp.DAL/Models/User.cs
+++ BlogApp.DAL/Models/User.cs
@@ -8,26 +8,24 @@ namespace BlogApp.DAL.Models
 {
     public class User
     {
-        [Key]
-        [Required]
-        [DataType(DataType.EmailAddress)]
-        public string UserId { get; set; }
-        [Required]
-        [Display(Name = "Username")]
-        public string Username { get; set; }
-        [Required]
-        [Display(Name = "Password")]
-        [DataType(DataType.Password)]
-        //[StringLength(100, ErrorMessage = "The {0} must be atleast {2} characters long.", MinimumLength = 8)]
-        //[MembershipPassword(MinRequiredNonAlphanumericCharacters = 1, MinNonAlphanumericCharactersError = "Your Password needs to contain atleast !,@,#,$ etc.", ErrorMessage = "Your Password must be 8 characters long and contain atleast one symbol(!,@,#,etc).")]
-        public string Password { get; set; }
-        [NotMapped]
-        [Compare("Password")]
-        [DataType(DataType.Password)]
-        [Required(ErrorMessage = "Confirm Password required")]
-        public string ConfirmPassword { get; set; }
-        [Required]
-        [Display(Name = "Role")]
-        public string Role { get; set; }
+        //[Key]
+        //[Required]
+        //[DataType(DataType.EmailAddress)]
+        //public string UserId { get; set; }
+        //[Required]
+        //[Display(Name = "Username")]
+        //public string Username { get; set; }
+        //[Required]
+        //[Display(Name = "Password")]
+        //[DataType(DataType.Password)]
+        //public string Password { get; set; }
+        //[NotMapped]
+        //[Compare("Password")]
+        //[DataType(DataType.Password)]
+        //[Required(ErrorMessage = "Confirm Password required")]
+        //public string ConfirmPassword { get; set; }
+        //[Required]
+        //[Display(Name = "Role")]
+        //public string Role { get; set; }
     }
 }
