4582c1a2d35febe5980caf64ffdf25a62f14b93c
faizanali22 <muhammadfaizan.wdc@Outlook.com>
2022-08-20 19:31:24 +0800

    Login and Registration UI added

--- BlogApp.Model/UserLoginViewModel.cs
+++ BlogApp.Model/UserLoginViewModel.cs
@@ -5,4 +5,13 @@
         public string Username { get; set; }
         public string Password { get; set; }
     }
+    public class UserRegisterViewModel
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
 }
